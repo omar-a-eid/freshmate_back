946501e748c2e65068bdee0caf84105e4928ce0f
karim Mohamed <karimdesouki147@gmail.com>
2024-04-20 18:31:00 +0200

    get product by id validation

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,22 +1,29 @@
 import productModel from "../models/productModel.js";
 
 
-export async function GetProducts(req,res){}
-export async function AddProduct(req,res){}
+export async function GetProducts(req, res) { }
+export async function AddProduct(req, res) { }
 // -----------
 // if i added another 0 on the id it gives an error not the invalid message why omar ??
 // -----------
-export async function GetProductsById(req,res){
-    // get id by params
-    let ProductId = req.params.id;
-    // findone the particularid in the DB
-    let foundProduct = await productModel.findOne({ _id: ProductId })
-    if (!foundProduct) {//if not found in DB
-        return res.send("invalid Product id");
+export async function GetProductsById(req, res) {
+    //add try and catch to handle the error 
+    try {
+        // get id by params
+        const ProductId = req.params.id;
+        // findone the particularid in the DB
+        const foundProduct = await productModel.findOne({ _id: ProductId })
+        if (!foundProduct) {//if not found in DB
+            return res.send("invalid Product id");
+        }
+        return (res.json({ message: `Product ${ProductId} Founded`, data: foundProduct }))
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send("enternal server error");
     }
+
     //res print 
-    return (res.json({ message: `Product ${ProductId} Founded`, data: foundProduct }))
 }
-export async function UpdateProduct(req,res){}
-export async function DeleteProduct(req,res){}
+export async function UpdateProduct(req, res) { }
+export async function DeleteProduct(req, res) { }
 
