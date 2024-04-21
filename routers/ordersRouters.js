f2c2211a245b7c1681a62e4df57b1bc485031ee8
Adel <ahmeddadel9988@gmail.com>
2024-04-21 17:30:34 +0200

    Editing Order Part

--- routers/ordersRouters.js
+++ routers/ordersRouters.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import * as Ord_con from "../controllers/orderController.js";
+
 const router = Router();
 
-router.get("/");
-router.post("/");
-router.get("/:id");
-router.put("/:id");
-router.delete("/:id");
-
+router.get("/admin", Ord_con.GetAllOrders);
+router.post("/"); 
+router.get("/:id", Ord_con.GetOrdersById);
+router.put("/:id", Ord_con.UpdateOrders);
+router.delete("/:id"); 
 export default router;
