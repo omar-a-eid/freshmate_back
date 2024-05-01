6da5e7354169ce4a163eb7ac40f88d40dc23e3af
omar-a-eid <omerx9988@gmail.com>
2024-05-01 18:00:01 +0300

    Fixing validation

--- util/orderUtil.js
+++ util/orderUtil.js
@@ -2,7 +2,7 @@ export function mapStatus(status) {
   const statusMapping = {
     pending: { en: "pending", ar: "معلق" },
     accepted: { en: "accepted", ar: "مقبول" },
-    rejected: { en: "rejected", ar: "مرفوض " },
+    rejected: { en: "rejected", ar: "مرفوض" },
     معلق: { en: "pending", ar: "معلق" },
     مقبول: { en: "accepted", ar: "مقبول" },
     مرفوض: { en: "rejected", ar: "مرفوض" },
